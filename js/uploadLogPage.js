db9e4b1f55313e8bb9e894eaf6d1fff956475d9b
Elena Yan <elena.yan@studio.unibo.it>
2023-06-01 22:30:33 +0200

    feat: handle file input

--- js/uploadLogPage.js
+++ js/uploadLogPage.js
@@ -2,20 +2,23 @@ import {CustomHTMLElement} from "./customHTMLElement.js";
 
 const uploadFileTemplate = document.createElement('template');
 uploadFileTemplate.innerHTML = `
-<div class="m-5 row d-flex justify-content-center align-items-center">
-  <div class="col-1"></div>
-  <div class="col-10 my-5" id="me">
-      <label for="formFile" class="mx-5 mb-3 form-label">Upload the logging files in .json format generated for your agents</label>
+<div class="col-1"></div>
+<div class="m-5 col-10 row d-flex justify-content-center align-items-center">
+  <div>
+      <label for="formFile" class="mx-5 mb-3 form-label">Upload the logging files in .json format generated for your agents:</label>
       <input class="mx-5 form-control" type="file" id="formFile" accept=".json" multiple>
   </div>
-  <div class="col-1"></div>
+  
+  <div class="mt-5 d-flex justify-content-center align-items-center">
+    <a href="#agents" id="continue" type="button" class="btn btn-primary">Continue</a>
+  </div>
 </div>
+<div class="col-1"></div>
 `
 
 class UploadLogPage extends CustomHTMLElement {
     constructor() {
         super(uploadFileTemplate);
-        // this.addEventListener('change', (e) => handleFiles(e))
     }
 
    connectedCallback() {
@@ -23,11 +26,16 @@ class UploadLogPage extends CustomHTMLElement {
 
        const fileInput = this.shadowRoot.querySelector("#formFile")
 
-       fileInput.onchange = () => {
-           const selectedFiles = [...fileInput.files];
-           console.log(selectedFiles);
+       if (fileInput != null) {
+           fileInput.onchange = () => {
+               const selectedFiles = [...fileInput.files];
+               console.log(selectedFiles);
+           }
+       } else {
+           location.reload()
        }
-
    }
+
+
 }
-customElements.define('upload-file', UploadLogPage)
+customElements.define('upload-log', UploadLogPage)
