e39a55aeb19380a9f2e8c656233cd50fec74216a
Elena Yan <elena.yan@studio.unibo.it>
2023-06-10 17:04:52 +0200

    refactor: use hash router

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
+
+const router = createHashRouter([
+    {
+        path: "/*",
+        element: <App />,
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
