04abb2e6eca0ecd582fbaeeb940793dc57e52113
Elena Yan <elena.yan@studio.unibo.it>
2023-06-11 15:16:45 +0200

    refactor(homepage): refactor code

--- src/component/Agent.js
+++ src/component/Agent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import {Col, ListGroup, ListGroupItem, Row} from "react-bootstrap";
 
 function Agent(props) {
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Agent from "../component/Agent";
 
 function HomePage(props) {
