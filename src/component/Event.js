4a2f5fd8e7a73c75824c5081fae902d898d52d00
Elena Yan <elena.yan@studio.unibo.it>
2023-07-03 18:54:46 +0200

    feat(events): create event components

--- src/component/Event.js
+++ src/component/Event.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Event(props) {
     return (
-        <li className="timeline-item mb-5">
-            <h5 className="fw-bold">{props.event.message.type}</h5>
+        <div>
+            <h5 className="fw-bold">{props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')}</h5>
             <p className="text-muted mb-2 fw-bold">{props.event.message.log}</p>
             <p className="text-muted">Timestamp: {props.event.timestamp}</p>
-        </li>
+        </div>
     )
 }
 
--- src/pages/AgentEventsPage.js
+++ src/pages/AgentEventsPage.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { MDBContainer } from "mdb-react-ui-kit";
-import Event from "../component/Event";
+import EventComponents from "../component/event/EventComponents";
 
 function AgentEventsPage(props) {
+    console.log(props.agent && React.createElement(EventComponents[props.agent.log[1].message.type], {event: "event"}))
+
     return (
         <MDBContainer className="py-5">
             <ul className="timeline">
-                {props.agent.log.map(event => (
-                    <Event event={event}/>
+                {props.agent && props.agent.log.map(event => (
+                    <li className="timeline-item mb-5">
+                        {
+                            //todo: level
+                            React.cloneElement(EventComponents[event.message.type]["jason"], {event: event})
+                        }
+                    </li>
                 ))}
             </ul>
         </MDBContainer>
