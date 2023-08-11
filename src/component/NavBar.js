17574549b26576a29301fefc476838ba8940c871
Elena Yan <elena.yan@studio.unibo.it>
2023-08-11 10:09:44 +0200

    fix: navbar href

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <Navbar bg="light">
             <Container className="m-0 w-100">
-                <Navbar.Brand href="/">BDI Multi-Agent Systems Explanation</Navbar.Brand>
+                <Navbar.Brand href="/agent-explanation">BDI Multi-Agent Systems Explanation</Navbar.Brand>
             </Container>
         </Navbar>
     )
