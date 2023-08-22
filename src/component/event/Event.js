b5d919a0b2702be8fd7aa719fadb7e93f3727de7
Elena Yan <elena.yan@studio.unibo.it>
2023-08-22 13:50:47 +0200

    feat: organize event description

--- src/component/event/Event.js
+++ src/component/event/Event.js
@@ -5,10 +5,18 @@ function Event(props) {
     const string = (props.type + " " + props.description + " " + props.info + " " + props.timestamp).toLowerCase()
     const filter = props.filter.toLowerCase()
 
+    const description = [];
+    if (props.description) {
+        props.description.split("\n").forEach(item => {
+            description.push(item);
+            description.push(<br/>);
+        });
+    }
+
     if (string.includes(filter) && props.type) {
         return (
             <div>
-                <h5 className="fw-bold">{props.description}</h5>
+                <h6 className="fw-bold">{description}</h6>
                 <p className="text-muted fw-bold my-2">Event type: {props.type}</p>
                 <p className="text-muted my-1">{props.info && props.info}</p>
                 <p className="text-muted my-1">Timestamp: {props.timestamp}</p>
