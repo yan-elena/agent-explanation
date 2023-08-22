1c16e1515f7101300d830b447932a5b851d0841b
Elena Yan <elena.yan@studio.unibo.it>
2023-08-22 17:19:21 +0200

    feat: add reason for desire committed

--- src/component/event/bdi/DesireCommitted.js
+++ src/component/event/bdi/DesireCommitted.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Event from "../Event";
 
-function DesireCommitted(props) {
+function DesireCommitted(props) { //todo plans
 
     const type = "Desire committed";
-    const desire = props.event.message.event.intentionInfo.intendedMeansInfo.slice(-1)[0].trigger
+    const im = props.event.message.event.intentionInfo.intendedMeansInfo
+    const desire = im[0].trigger
+    const parentDesire = im[1] ? "Intention " + props.event.message.event.intentionInfo.id + ": " + desire + " is an intention created from the intention " + im[1].trigger : ""
     const selectedPlan = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "SelectPlanEvent" && e.message.event.event === desire).message.event.selectedPlan;
+    const context = selectedPlan.context ? " because I believe " + selectedPlan.context.replace("&", "and").replace("|", "or") : ""
 
-    const description = "I committed to desire " + desire + ", and it became a new intention"
-
-    console.log(selectedPlan)
-    console.log(props.event)
-    console.log("------")
-    //React.cloneElement(EventComponents[selectPlanEvent.message.type]["JASON"], {log: props.agent.log, event: selectPlanEvent, filter: props.filter})
+    const description = "I committed to desire " + desire + context + ", and it became a new intention"
 
     return (
-        <Event type={type} description={description} timestamp={props.event.timestamp} filter={props.filter}/>
+        <Event type={type} description={description} info={parentDesire} timestamp={props.event.timestamp} filter={props.filter}/>
     )
 }
 
