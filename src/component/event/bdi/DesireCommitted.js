5936e089a0b8da6930e7fb13879e3c4184eeeb55
Elena Yan <elena.yan@studio.unibo.it>
2023-08-25 18:09:49 +0200

    feat: desire committed

--- src/component/event/bdi/DesireCommitted.js
+++ src/component/event/bdi/DesireCommitted.js
@@ -11,26 +11,25 @@ function DesireCommitted(props) { //todo test goal
     const body = "Plan body: " + selectedPlan.body
     const [updated, setUpdated] = useState(false)
 
+    const intention = props.log.slice(props.log.indexOf(props.event)).find(e => e.message.type === "IntentionCreated" && e.message.event.intentionInfo.intendedMeansInfo[0].trigger === desire);
+    const im = intention.message.event.intentionInfo.intendedMeansInfo
+    const id = intention.message.event.intentionInfo.id
+    const parentDesire = im[1] ? ["Intention " + id + ": " + desire + " is an intention created from the intention " + im[1].trigger, <br/>] : []
+    const description = "I committed to desire " + desire + context + ", and it became a new intention"
+
     useEffect(() => {
-        if (!agentState.intention.includes(desire)) {
-            agentState.intention.push(desire)
+        const i = desire + "/" + id
+        if (!agentState.intention.includes(i)) {
+            agentState.intention.push(i)
             setUpdated(true)
         }
     }, [])
 
-    const intention = props.log.slice(props.log.indexOf(props.event)).find(e => e.message.type === "IntentionCreated" && e.message.event.intentionInfo.intendedMeansInfo[0].trigger === desire);
-
-    if (intention != null) {
-        const im = intention.message.event.intentionInfo.intendedMeansInfo
-        const parentDesire = im[1] ? ["Intention " + intention.message.event.intentionInfo.id + ": " + desire + " is an intention created from the intention " + im[1].trigger, <br/>] : []
-        const description = "I committed to desire " + desire + context + ", and it became a new intention"
-
-        if (updated) {
-            return (
-                <Event type={type} description={description} info={[...parentDesire, body]} timestamp={props.event.timestamp}
-                       filter={props.filter}/>
-            )
-        }
+    if (updated) {
+        return (
+            <Event type={type} description={description} info={[...parentDesire, body]} timestamp={props.event.timestamp}
+                   filter={props.filter}/>
+        )
     }
 }
 
