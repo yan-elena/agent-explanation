901cad1c1e98f27804c2fd7f35ba415d9c7158df
Elena Yan <elena.yan@studio.unibo.it>
2023-08-30 10:47:14 +0200

    feat: highlight in filtered events

--- src/component/event/Event.js
+++ src/component/event/Event.js
@@ -6,20 +6,42 @@ function Event(props) {
     const filter = props.filter.toLowerCase()
 
     const description = [];
+    const highlightMatchingText = (text, matchingText) => {
+        if (text && matchingText) {
+            const matchRegex = RegExp(matchingText, 'ig');
+
+            // Matches array needed to maintain the correct letter casing
+            if (text.matchAll?.(matchRegex)) {
+                const matches = [...text.matchAll?.(matchRegex)];
+
+                return text
+                    .split(matchRegex)
+                    .map((nonBoldText, index, arr) => (
+                        <React.Fragment key={index}>
+                            {nonBoldText}
+                            {index + 1 !== arr.length && <mark>{matches[index]}</mark>}
+                        </React.Fragment>
+                    ));
+            }
+        }
+        return text
+    };
+
     if (props.description) {
         props.description.split("\n").forEach(item => {
-            description.push(item);
+            description.push(highlightMatchingText(item, filter));
             description.push(<br/>);
         });
+
     }
 
     if (string.includes(filter) && props.type) {
         return (
             <div>
                 <h6 className="fw-bold">{description}</h6>
-                <p className="text-muted fw-bold my-2">Event type: {props.type}</p>
-                <p className="text-muted my-1">{props.info && props.info}</p>
-                <p className="text-muted my-1">Timestamp: {props.timestamp}</p>
+                <p className="text-muted fw-bold my-2">{highlightMatchingText("Event type: " + props.type, filter)}</p>
+                <p className="text-muted my-1">{props.info && highlightMatchingText(props.info, filter)}</p>
+                <p className="text-muted my-1">{highlightMatchingText("Timestamp: " + props.timestamp, filter)}</p>
             </div>
         )
     } else {
