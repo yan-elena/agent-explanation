947207a491dcbe1d1142ffcfca8b3fb44e3eadde
Elena Yan <elena.yan@studio.unibo.it>
2023-08-30 10:46:33 +0200

    refactor: refactor intention information in desire events

--- src/component/event/bdi/DesireCommitted.js
+++ src/component/event/bdi/DesireCommitted.js
@@ -13,8 +13,17 @@ function DesireCommitted(props) {
     const intention = props.log.slice(props.log.indexOf(props.event)).find(e => e.message.type === "IntentionCreated" && e.message.event.intentionInfo.intendedMeansInfo[0].trigger === desire);
     const im = intention.message.event.intentionInfo.intendedMeansInfo
     const id = intention.message.event.intentionInfo.id
-    const parentDesire = im[1] ? ["Intention " + desire + "/" + id + " is an intention created from intention " + im[1].trigger + "/" + id, <br/>] : []
     const description = "I committed to desire " + desire + context + ", and it became a new intention " + desire + "/" + id
+    let parentDesire  = []
+
+    if (im[0]) {
+        if (im[0].trigger !== desire) {
+            parentDesire = ["Intention " + desire + "/" + id + " is an intention created from intention " + im[0].trigger + "/" + id,
+                <br/>]
+        } else {
+            parentDesire = im[1] ? ["Intention " + desire + "/" + id + " is an intention created from intention " + im[1].trigger + "/" + id, <br/>] : []
+        }
+    }
 
     if (agentState.intention[id]) {
         agentState.intention[id].push(desire)
--- src/component/event/bdi/NewDesire.js
+++ src/component/event/bdi/NewDesire.js
@@ -2,19 +2,19 @@ import React from "react";
 import Event from "../Event";
 
 function NewDesire(props) {
-    const type = props.event.message.type
     const eventType = "New Desire"
     const functor = props.event.message.event.goalInfo.goalFunctor
     const intention = props.event.message.event.goalInfo.intention.value
     const state = props.event.message.event.goalStates
     const info = "State: " + state
 
-    let description = ""
+    let description = "I have a new desire " + functor
 
-    if (type === "GoalCreated") {
-        description = "I have a new desire " + functor + " because " +
-            (intention === null || !intention.intendedMeansInfo.length > 0 ? " it is an initial desire"
-                : " it is a desire created from " + intention.intendedMeansInfo.slice(-1)[0].trigger)
+    if (intention) {
+        let im = intention.intendedMeansInfo
+        description = description + " because " + (!im.length > 0 ? " it is an initial desire" : " it is a desire created from " + im[0].trigger)
+    } else {
+        description = description + " because it is an initial desire"
     }
 
     return (
