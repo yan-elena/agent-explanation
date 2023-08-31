af051c3d115ec7b9df63c569f267005d151f347f
Elena Yan <elena.yan@studio.unibo.it>
2023-08-31 07:59:58 +0200

    feat: add percept information in new percept belief

--- src/component/event/bdi/NewBelief.js
+++ src/component/event/bdi/NewBelief.js
@@ -8,13 +8,16 @@ function NewBelief(props) {
     const belief = beliefEvent.literal
     const source = beliefEvent.source.value ? beliefEvent.source.value : ""
     let reason
+    let info
 
     switch (String(source)) {
         case "self":
             reason = " because I noted it in my mind for future reference"
             break;
         case "percept":
-            reason = " because I perceived it"
+            const percept = props.log.slice(props.log.indexOf(props.event)).find(e => e.message.type === "NewPercept" && e.message.event.perceptInfo.functor === belief).message.event.perceptInfo;
+            reason = " because I perceived it from " +  percept.artifactName
+            info = "Percept type: " + percept.perceptType
             break;
         case "":
             reason = ""
@@ -27,7 +30,7 @@ function NewBelief(props) {
     const description = "I believe " + belief + reason
 
     return (
-        <Event type={eventType} description={description} timestamp={props.event.timestamp} filter={props.filter}/>
+        <Event type={eventType} description={description} info={info} timestamp={props.event.timestamp} filter={props.filter}/>
     )
 }
 
