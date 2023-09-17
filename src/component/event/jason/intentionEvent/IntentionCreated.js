c392acab4c397cde68c09c043878410c9faf7200
Elena Yan <elena.yan@studio.unibo.it>
2023-09-17 12:47:45 +0200

    feat: intention created unifier information

--- src/component/event/jason/intentionEvent/IntentionCreated.js
+++ src/component/event/jason/intentionEvent/IntentionCreated.js
@@ -2,9 +2,15 @@ import React from "react";
 import Event from "../../Event";
 
 function IntentionCreated(props) {
+
+    const im = props.event.message.event.intentionInfo.intendedMeansInfo
+
     return (
         <div>
-            <Event type={props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')} description={props.event.message.log} timestamp={props.event.timestamp} filter={props.filter}/>
+            <Event type={props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')}
+                   description={props.event.message.log}
+                   info={im.length > 0 && im[0].unifier ? "Unifier: " + im[0].unifier : ""}
+                   timestamp={props.event.timestamp} filter={props.filter}/>
         </div>
     )
 }
