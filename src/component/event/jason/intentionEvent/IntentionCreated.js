49e9e17396d15a7b5d74fadf18ceebfb8ab9384c
Elena Yan <elena.yan@studio.unibo.it>
2023-10-04 07:35:46 +0200

    refactor: refactor intention unifier

--- src/component/event/jason/intentionEvent/IntentionCreated.js
+++ src/component/event/jason/intentionEvent/IntentionCreated.js
@@ -4,12 +4,13 @@ import Event from "../../Event";
 function IntentionCreated(props) {
 
     const im = props.event.message.event.intentionInfo.intendedMeansInfo
+    const unifier = im[0].unifier
 
     return (
         <div>
             <Event type={props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')}
                    description={props.event.message.log}
-                   info={im.length > 0 && im[0].unifier ? "Unifier: " + im[0].unifier : ""}
+                   info={im.length > 0 && unifier && unifier !== "{}" ? "Unifier: " + unifier : ""}
                    timestamp={props.event.timestamp} filter={props.filter}/>
         </div>
     )
