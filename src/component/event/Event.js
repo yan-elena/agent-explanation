ca555554f496c6df23611c860e19003004e1d018
Elena <elena.yan@studio.unibo.it>
2024-04-28 16:56:13 +0200

    feat: add explanation link option and page

--- src/component/event/Event.js
+++ src/component/event/Event.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button} from "react-bootstrap";
+import EventExplanationPage from "../../pages/EventExplanationPage";
 
 function Event(props) {
 
     const string = (props.type + " " + props.description + " " + props.info + " " + props.timestamp).toLowerCase()
     const filter = props.filter.toLowerCase()
 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        console.log(props.explanation)
+        setShow(true)
+    }
+
     const description = [];
-    const highlightMatchingText = (text, matchingText) => {
+     const highlightMatchingText = (text, matchingText) => {
         if (text && matchingText) {
             const matchRegex = RegExp(matchingText.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), 'ig');
 
@@ -37,10 +46,12 @@ function Event(props) {
     if (string.includes(filter) && props.type) {
         return (
             <div>
+                <EventExplanationPage show={show} handleClose={handleClose} event={props.description} explanation={props.explanation} log={props.log} level={props.level}/>
                 <h6 className="fw-bold">{description}</h6>
                 <p className="text-muted fw-bold my-2">{highlightMatchingText("Event type: " + props.type, filter)}</p>
                 <p className="text-muted my-1">{props.info && (Array.isArray(props.info) ? [props.info.map(info => highlightMatchingText(info, filter))] : highlightMatchingText(props.info, filter))}</p>
                 <p className="text-muted my-1">{highlightMatchingText("Timestamp: " + props.timestamp, filter)}</p>
+                {props.explanation && props.explanation.length > 0 ? <Button variant="outline-primary" size="sm" onClick={handleShow}>View explanation</Button> : <></>}
             </div>
         )
     } else {
---
+++ src/pages/EventExplanationPage.js
@@ -0,0 +1,37 @@
+import {MDBContainer} from "mdb-react-ui-kit";
+import React from "react";
+import EventMapping from "../component/event/EventMapping";
+import {Modal} from "react-bootstrap";
+
+function EventExplanationPage(props) {
+    if (props.explanation) {
+        console.log(props.explanation[0])
+        return (
+            <Modal show={props.show} onHide={props.handleClose}>
+                <Modal.Header>
+                    <h6 className="fw-bold">Explanation of the event: "{props.event}"</h6>
+                </Modal.Header>
+                <Modal.Body>
+                    <MDBContainer xs={12} className="my-5">
+                        <ul className="timeline">
+                            {props.explanation.map((event, i) => (
+                                <li className="timeline-item mb-5" key={event.timestamp + "_" + i}>
+                                    {React.cloneElement(EventMapping[event.message.type][props.level], {
+                                        log: props.log,
+                                        event: event,
+                                        filter: "",
+                                        level: props.level
+                                    })}
+                                </li>
+                            ))}
+                        </ul>
+                    </MDBContainer>
+                </Modal.Body>
+            </Modal>
+        )
+    } else {
+        return (<></>)
+    }
+}
+
+export default EventExplanationPage;
