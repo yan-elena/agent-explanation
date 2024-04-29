1b1d7c1c492b2f47de778471a9dcb37d814d6ffd
Elena <elena.yan@studio.unibo.it>
2024-04-29 17:55:00 +0200

    feat: belief removed explanation link

--- src/component/event/design/BeliefRemoved.js
+++ src/component/event/design/BeliefRemoved.js
@@ -13,15 +13,17 @@ function BDIBeliefRemoved(props) {
     let beliefAdded = getCycleEvents(props.log, props.log.indexOf(props.event)).find(e => e.message.type === "BeliefAdded" && e.message.event.beliefInfo.functor === beliefEvent.functor);
     const intention = props.log.slice(0, props.log.indexOf(props.event)).find(e => e.message.type === "IntentionCreated" && e.message.event.intentionInfo.intendedMeansInfo[0].plan.body.includes(belief));
     let reason = ""
+    const explanation = []
 
     if (intention) {
         const intentionInfo = intention.message.event.intentionInfo
         reason = " because of intention " + intentionInfo.id + " " + intentionInfo.intendedMeansInfo[0].trigger
+        explanation.push(intention)
     }
 
     if (!beliefAdded && !(beliefDeed && beliefDeed.message.type === "ExecutedDeed" && beliefDeed.message.event.deedInfo.type === "delAddBel" && beliefDeed.message.event.deedInfo.term.includes(beliefEvent.functor))) {
         return (
-            <Event type={type} description={description + reason} timestamp={props.event.timestamp} filter={props.filter} log={props.log} level={Level.DESIGN}/>
+            <Event type={type} description={description + reason} timestamp={props.event.timestamp} filter={props.filter} log={props.log} level={Level.DESIGN} explanation={explanation}/>
         )
     }
 }
