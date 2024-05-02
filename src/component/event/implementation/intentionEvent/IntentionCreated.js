9fd6fcbd4ffaba20c5a45cf5ff498bb74cc31475
Elena Yan <elena.yan@studio.unibo.it>
2024-05-02 17:34:47 +0200

    refactor: intention created at implementation level

--- src/component/event/implementation/intentionEvent/IntentionCreated.js
+++ src/component/event/implementation/intentionEvent/IntentionCreated.js
@@ -8,17 +8,18 @@ function IntentionCreated(props) {
     const unifier = im[0].unifier
     const explanation = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "GoalCreated" && e.message.event.goalInfo.intention.value && e.message.event.goalInfo.intention.value.id === props.event.message.event.intentionInfo.id)
 
-    return (
-        <div>
-            <Event type={props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')}
-                   description={props.event.message.log}
-                   info={im.length > 0 && unifier && unifier !== "{}" ? "Unifier: " + unifier : ""}
-                   timestamp={props.event.timestamp} filter={props.filter}
-                   log={props.log}
-                   level={Level.IMPLEMENTATION}
-                   explanation={explanation}/>
-        </div>
-    )
-}
+    if (props.event.message.event.intentionInfo.state === "undefined") {
+        return (
+            <div>
+                <Event type={props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')}
+                    description={props.event.message.log}
+                    info={im.length > 0 && unifier && unifier !== "{}" ? "Unifier: " + unifier : ""}
+                    timestamp={props.event.timestamp} filter={props.filter}
+                    log={props.log}
+                    level={Level.IMPLEMENTATION}
+                    explanation={explanation}/>
+            </div>
+        )
+    }
 
 export default IntentionCreated;
