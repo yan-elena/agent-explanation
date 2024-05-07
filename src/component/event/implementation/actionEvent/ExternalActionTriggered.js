568a62943916aee795afe12bb9d63f371db82179
Elena <elena.yan@studio.unibo.it>
2024-05-07 23:15:46 +0200

    feat: external action explanation link

--- src/component/event/implementation/actionEvent/ExternalActionTriggered.js
+++ src/component/event/implementation/actionEvent/ExternalActionTriggered.js
@@ -5,9 +5,9 @@ import {Level} from "../../../../model/Level";
 function ExternalActionTriggered(props) {
     const deed = props.event.message.event.action
     const type = "External Action Triggered"
-    const intention = props.event.message.event.intentionInfo.value
+    const intention = props.event.message.event.action.intention
     let description = "External action "  + deed.term + " triggered"
-    const explanation = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "GoalCreated" && intention && e.message.event.goalInfo.goalFunctor === intention.trigger)
+    const explanation = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "GoalCreated" && intention && e.message.event.goalInfo.intention.value && e.message.event.goalInfo.intention.value.id === intention.id)
     const info = "Type: external action" + (intention ? ", Intention: " + intention.id + " " + intention.trigger : "")
 
     return (
