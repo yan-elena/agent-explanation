db680f75cf0a4e0fda33b82250f41cf2854ae2f8
Elena <elena.yan@studio.unibo.it>
2024-06-02 19:49:41 +0200

    refactor: select plan narrative

--- src/component/event/design/SelectPlan.js
+++ src/component/event/design/SelectPlan.js
@@ -6,9 +6,19 @@ function SelectPlan(props) {
 
     const event = props.event.message
     const explanation = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "GoalCreated" && e.message.event.goalInfo.goalFunctor === event.event.event.split('[')[0])
+    const selectPlan = event.event
+    const trigger = selectPlan.event
+    let description = "I am choosing the plan for "
+
+    if (trigger.includes("error(no_applicable)")) {
+        description += "the failure of "
+    }
+
+    description = description + trigger.split(')[')[0] + ") from: \n" + selectPlan.planOptions.map(p => "\t"+p.log).join("\n")
+    description = description + "\n\nI selected the plan @" + selectPlan.selectedPlan.label
 
     return (
-        <Event type={event.type} description={event.log} timestamp={props.event.timestamp} filter={props.filter} log={props.log} level={Level.DESIGN} explanation={explanation}/>
+        <Event type={event.type} description={description} timestamp={props.event.timestamp} filter={props.filter} log={props.log} level={Level.DESIGN} explanation={explanation}/>
     )
 }
 
