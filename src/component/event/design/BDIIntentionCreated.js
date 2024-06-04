8e25bf4a1cca916226d102294cfb37fa7601f241
Elena <elena.yan@studio.unibo.it>
2024-06-04 12:09:21 +0200

    refactor: explanation od the intention created

--- src/component/event/design/BDIIntentionCreated.js
+++ src/component/event/design/BDIIntentionCreated.js
@@ -8,8 +8,7 @@ function BDIIntentionCreated(props) {
     const intention = event.message.event.intentionInfo
     const goal = intention.intendedMeansInfo[0].plan.trigger
     const id = "int-" + intention.id + "-" + goal
-    const explanation = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "GoalCreated" && e.message.event.goalInfo.intention.value && e.message.event.goalInfo.intention.value.id === intention.id)
-    //const selectPlan = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "PlanSelected" && e.message.event.goalInfo.goalFunctor === goal)
+    const explanation = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "GoalCreated" && e.message.event.goalInfo.goalFunctor.includes(intention.intendedMeansInfo[0].trigger))
     const selectPlan = props.log[props.log.indexOf(props.event)-1]
     const description = "I have a new intention " + id
 
