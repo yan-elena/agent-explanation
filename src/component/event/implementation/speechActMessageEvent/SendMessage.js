d9ac20daf9f8323d00e3cd0089d753e92f7bb6b3
Elena <elena.yan@studio.unibo.it>
2024-06-04 11:46:25 +0200

    feature: add explanation in send message

--- src/component/event/implementation/speechActMessageEvent/SendMessage.js
+++ src/component/event/implementation/speechActMessageEvent/SendMessage.js
@@ -3,9 +3,14 @@ import Event from "../../Event";
 import {Level} from "../../../../model/Level";
 
 function SendMessage(props) {
+
+    const intention = props.event.message.event.intentionInfo.intendedMeansInfo[0].trigger
+    const explanation = props.log.slice(0, props.log.indexOf(props.event)).findLast(e => e.message.type === "GoalCreated" && e.message.event.goalInfo.goalFunctor.includes(intention))
+
     return (
         <div>
-            <Event type={props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')} description={props.event.message.log} timestamp={props.event.timestamp} filter={props.filter} log={props.log} level={Level.IMPLEMENTATION}/>
+            <Event type={props.event.message.type.replace(/\B([A-Z])\B/g, ' $1')} description={props.event.message.log}
+                   explanation={explanation} timestamp={props.event.timestamp} filter={props.filter} log={props.log} level={Level.IMPLEMENTATION}/>
         </div>
     )
 }
