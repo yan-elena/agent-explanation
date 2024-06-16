3934966999b7d986b9d9203a33c3c6e06f719b02
Elena <elena.yan@studio.unibo.it>
2024-06-16 22:43:35 +0200

    refactor: update navbar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <Navbar bg="light">
             <Container className="m-0 w-100">
-                <Navbar.Brand href="/agent-explanation">BDI Multi-Agent Systems Explanation</Navbar.Brand>
+                <Navbar.Brand href="/agent-explanation">A Multi Level Explainability Framework for BDI Agents</Navbar.Brand>
             </Container>
         </Navbar>
     )
